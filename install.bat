2852fdf45223c10a92e0685b5386991c6be6467e
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2023-09-27 13:15:11 +0530

    Improve install script

--- install.bat
+++ install.bat
@@ -1,5 +1,12 @@
-mkdir %userprofile%\.yen\bin
+@echo off
+
+SET yenpath=%userprofile%\.yen\bin
+mkdir %yenpath%
+
 SET download_url="https://github.com/tusharsadhwani/yen/releases/download/yen-x86_64-pc-windows-msvc"
-curl -SL --progress-bar %download_url% --output %userprofile%\.yen\bin\yen.exe
-setx PATH "%PATH%;%uerprofile\.yen\bin"
+curl -SL --progress-bar %download_url% --output %yenpath%\yen.exe
+
+:: Set PATH if not present
+if "%PATH:.yen\bin=repl%"=="%PATH%" setx PATH "%PATH%;%yenpath%"
+
 echo Successfully installed yen! Restart the shell to start using the 'yen' command.
