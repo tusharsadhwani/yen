50749dcdf484463c5b114a508d377ce36a5d5942
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2024-06-22 01:54:10 +0530

    whoops

--- tests/yen_test.py
+++ tests/yen_test.py
@@ -38,7 +38,7 @@ def run(command: list[str]) -> str:
     try:
         output = subprocess.check_output(command, stderr=subprocess.STDOUT).decode()
     except subprocess.CalledProcessError as exc:
-        print(f"Subprocess output: {exc.output.decode(errors="ignore")}")
+        print(f"Subprocess output: {exc.output.decode(errors='ignore')}")
         raise
 
     return output
