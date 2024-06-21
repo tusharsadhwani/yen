1b477dddc1ac278d9b3e1225946e7672f3aef14c
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2024-06-22 03:42:34 +0530

    Add mvp for yen install and yen run

--- src/yen/__init__.py
+++ src/yen/__init__.py
@@ -5,27 +5,33 @@ from __future__ import annotations
 import hashlib
 import os
 import os.path
+import platform
 import subprocess
+import sys
 import tarfile
 
 from yen.downloader import download, read_url
-from yen.github import NotAvailable, resolve_python_version
+from yen.github import resolve_python_version
 
-PYTHON_INSTALLS_PATH = os.getenv("YEN_PYTHONS_PATH") or os.path.expanduser(
-    "~/.yen_pythons"
+PYTHON_INSTALLS_PATH = os.getenv(
+    "YEN_PYTHONS_PATH", os.path.expanduser("~/.yen_pythons")
+)
+PACKAGE_INSTALLS_PATH = os.getenv(
+    "YEN_PACKAGES_PATH", os.path.expanduser("~/.yen_packages")
 )
 
 
-def check_path() -> None:
-    """Ensure that PYTHON_INSTALLS_PATH is in PATH."""
-    if PYTHON_INSTALLS_PATH not in os.environ["PATH"]:
+def check_path(path: str) -> None:
+    """Ensure that given path is in PATH."""
+    if path not in os.environ["PATH"].split(os.pathsep):
         print(
             "\033[33m\n"
-            "Warning: PYTHON_INSTALLS_PATH is not in PATH.\n"
+            "Warning: The executable just installed is not in PATH.\n"
             "Add the following line to your shell's configuration file:\n"
             "\033[0;1m"
-            f"export PATH={PYTHON_INSTALLS_PATH}:$PATH"
-            "\033[m"
+            f"export PATH={path}:$PATH"
+            "\033[m",
+            file=sys.stderr,
         )
 
 
@@ -58,7 +64,7 @@ def ensure_python(python_version: str) -> tuple[str, str]:
     checksum_link = download_link + ".sha256"
     expected_checksum = read_url(checksum_link).rstrip("\n")
     if checksum != expected_checksum:
-        print(f"\033[1;31mError:\033[m Checksum did not match!")
+        print("\033[1;31mError:\033[m Checksum did not match!")
         os.remove(downloaded_filepath)
         raise SystemExit(1)
 
@@ -71,22 +77,58 @@ def ensure_python(python_version: str) -> tuple[str, str]:
     return python_version, python_bin_path
 
 
-def create_venv(python_version: str, python_bin_path: str, venv_path: str) -> None:
-    if os.path.exists(venv_path):
+def create_venv(python_bin_path: str, venv_path: str, exists_ok: bool = False) -> bool:
+    """if `exist_ok` is True, Returns False if venv already existed."""
+    if os.path.exists(venv_path) and not exists_ok:
         print(f"\033[1;31mError:\033[m {venv_path} already exists.")
         raise SystemExit(2)
 
     subprocess.run([python_bin_path, "-m", "venv", venv_path], check=True)
-    print(f"Created \033[1m{venv_path}\033[m with Python {python_version} ✨".encode())
+
+
+def _venv_binary_path(binary_name: str, venv_path: str) -> str:
+    is_windows = platform.system() == "Windows"
+    venv_bin_path = os.path.join(venv_path, "Scripts" if is_windows else "bin")
+    binary_path = os.path.join(
+        venv_bin_path, f"{binary_name}.exe" if is_windows else binary_name
+    )
+    return binary_path
+
+
+def install_package(package_name: str, python_bin_path: str) -> tuple[str, bool]:
+    # TODO: add `force` arg to manually remove the venv if it exists
+    # TODO: add `--spec` equivalent from pipx
+    # TODO: maybe add a `--module` flag that runs it as `python -m package_name` instead
+    os.makedirs(PACKAGE_INSTALLS_PATH, exist_ok=True)
+    venv_name = f"venv_{package_name}"
+    venv_path = os.path.join(PACKAGE_INSTALLS_PATH, venv_name)
+    venv_created = create_venv(python_bin_path, venv_path, exists_ok=True)
+
+    if not venv_created:
+        return venv_path, True  # True as in already existed
+
+    venv_python_path = _venv_binary_path("python", venv_path)
+    subprocess.run([venv_python_path, "-m", "pip", "install", package_name], check=True)
+    package_bin_path = _venv_binary_path(package_name, venv_path)
+    os.symlink(package_bin_path, os.path.join(PACKAGE_INSTALLS_PATH, package_name))
+
+    check_path(PACKAGE_INSTALLS_PATH)
+    return venv_path, False
+
+
+def run_package(package_name: str, venv_path: str, args: list[str]) -> None:
+    # TODO: add `--spec` equivalent from pipx
+    # TODO: maybe add a `--module` flag that runs it as `python -m package_name` instead
+    package_bin_path = _venv_binary_path(package_name, venv_path)
+    subprocess.run([package_bin_path, *args])
 
 
 def create_symlink(python_bin_path: str, python_version: str) -> None:
-    python_major_minor = "python" + ".".join(python_version.split(".")[:2])
-    symlink_path = os.path.join(PYTHON_INSTALLS_PATH, python_major_minor)
+    python_version = "python" + ".".join(python_version.split(".")[:2])
+    symlink_path = os.path.join(PYTHON_INSTALLS_PATH, python_version)
 
     if os.path.exists(symlink_path):
         os.remove(symlink_path)
 
     os.symlink(python_bin_path, symlink_path)
-    print(f"\033[1m{python_major_minor}\033[m created in {PYTHON_INSTALLS_PATH} 🐍")
-    check_path()
+    check_path(PYTHON_INSTALLS_PATH)
--- src/yen/cli.py
+++ src/yen/cli.py
@@ -6,14 +6,16 @@ import argparse
 import sys
 from typing import Literal
 
-from yen import create_symlink, create_venv, ensure_python
+from yen import create_symlink, create_venv, ensure_python, install_package, run_package
 from yen.github import NotAvailable, list_pythons
 
 
 class YenArgs:
-    command: Literal["list", "create", "use"]
+    command: Literal["list", "create", "install", "run", "use"]
     python: str
     venv_path: str
+    package_name: str
+    command_args: list[str]
 
 
 def cli() -> int:
@@ -27,8 +29,17 @@ def cli() -> int:
     create_parser.add_argument("venv_path")
     create_parser.add_argument("-p", "--python", required=True)
 
-    create_parser = subparsers.add_parser("use")
-    create_parser.add_argument("-p", "--python", required=True)
+    install_parser = subparsers.add_parser("install")
+    install_parser.add_argument("package_name")
+    install_parser.add_argument("-p", "--python", default="3.12")
+
+    run_parser = subparsers.add_parser("run")
+    run_parser.add_argument("package_name")
+    run_parser.add_argument("-p", "--python", default="3.12")
+    run_parser.add_argument("command_args", nargs="+")
+
+    use_parser = subparsers.add_parser("use")
+    use_parser.add_argument("-p", "--python", required=True)
 
     args = parser.parse_args(namespace=YenArgs)
 
@@ -41,7 +52,6 @@ def cli() -> int:
     elif args.command == "create":
         try:
             python_version, python_bin_path = ensure_python(args.python)
-            create_venv(python_version, python_bin_path, args.venv_path)
         except NotAvailable:
             print(
                 "Error: requested Python version is not available."
@@ -50,15 +60,59 @@ def cli() -> int:
             )
             return 1
 
-    elif args.command == "use":
+        create_venv(python_bin_path, args.venv_path)
+        print(
+            f"Created \033[1m{args.venv_path}\033[m" f" with Python {python_version} ✨"
+        )
+
+    elif args.command == "install":
         try:
             python_version, python_bin_path = ensure_python(args.python)
-            create_symlink(python_bin_path, python_version)
         except NotAvailable:
             print(
                 "Error: requested Python version is not available."
-                " Use 'yen list' to get list of available Pythons."
+                " Use 'yen list' to get list of available Pythons.",
+                file=sys.stderr,
             )
             return 1
+
+        # TODO: add yaspin?
+        already_installed = install_package(args.package_name, python_bin_path)
+        if already_installed:
+            print(f"Package \033[1m{args.package_name}\033[m is already installed.")
+        else:
+            print(
+                f"Installed package \033[1m{args.package_name}\033[m"
+                f" with Python {python_version} ✨"
+            )
+
+    elif args.command == "run":
+        try:
+            _, python_bin_path = ensure_python(args.python)
+        except NotAvailable:
+            print(
+                "Error: requested Python version is not available."
+                " Use 'yen list' to get list of available Pythons.",
+                file=sys.stderr,
+            )
+            return 1
+
+        # TODO: add yaspin?
+        venv_path, _ = install_package(args.package_name, python_bin_path)
+        run_package(args.package_name, venv_path, args.command_args)
+
+    elif args.command == "use":
+        try:
+            python_version, python_bin_path = ensure_python(args.python)
+        except NotAvailable:
+            print(
+                "Error: requested Python version is not available."
+                " Use 'yen list' to get list of available Pythons.",
+                file=sys.stderr,
+            )
+            return 1
+
+        create_symlink(python_bin_path, python_version)
+        print(f"\033[1m{python_version}\033[m created 🐍")
 
     return 0
