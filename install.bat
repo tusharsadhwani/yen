26ee9ccf6b760965cd5ef6b0e128df6ac04e44cd
Gregory-K <lifeturn@gmail.com>
2024-06-24 11:36:02 +0200

    Update Windows Installation Methods (#16)

--- install.bat
+++ install.bat
@@ -1,12 +1,30 @@
 @echo off
+setlocal enabledelayedexpansion
 
-SET yenpath=%userprofile%\.yen\bin
-mkdir %yenpath%
+REM Set yenpath variable to the user's .yen\bin directory
+SET "yenpath=%userprofile%\.yen\bin"
 
-SET download_url=https://github.com/tusharsadhwani/yen/releases/latest/download/yen-rs-x86_64-pc-windows-msvc.exe
-curl -SL --progress-bar %download_url% --output %yenpath%\yen.exe
+REM Create the .yen\bin directory if it doesn't exist
+mkdir "%yenpath%" 2>nul
 
-:: Set PATH if not present
-if "%PATH:.yen\bin=repl%"=="%PATH%" setx PATH "%PATH%;%yenpath%"
+REM Download yen executable and save it to the .yen\bin directory
+SET "download_url=https://github.com/tusharsadhwani/yen/releases/latest/download/yen-rs-x86_64-pc-windows-msvc.exe"
+curl -SL --progress-bar "%download_url%" --output "%yenpath%\yen.exe"
+
+REM Get the user's PATH without the system-wide PATH
+for /f "skip=2 tokens=2,*" %%A in ('reg query HKCU\Environment /v PATH') do (
+    set "UserPath=%%B"
+    goto :Continue
+)
+:Continue
+
+REM Check if the directory .yen\bin exists in the user's PATH
+echo !UserPath! | findstr /i /c:".yen\bin" >nul
+if errorlevel 1 (
+    REM If it doesn't exist, append !yenpath! to the user's PATH
+    setx PATH "!UserPath!;!yenpath!"
+)
+
+endlocal
 
 echo Successfully installed yen! Restart the shell to start using the 'yen' command.
---
+++ install.ps1
@@ -0,0 +1,23 @@
+# Set yenpath variable to the user's .yen\bin directory
+$yenpath = "$env:userprofile\.yen\bin"
+
+# Create the .yen\bin directory if it doesn't exist
+if (-not (Test-Path $yenpath)) {
+    New-Item -Path $yenpath -ItemType Directory | Out-Null
+}
+
+# Download yen executable and save it to the .yen\bin directory
+$downloadUrl = "https://github.com/tusharsadhwani/yen/releases/latest/download/yen-rs-x86_64-pc-windows-msvc.exe"
+Invoke-WebRequest -Uri $downloadUrl -OutFile "$yenpath\yen.exe"
+
+# Get the user's PATH without the system-wide PATH
+$userPath = (Get-ItemProperty -Path 'HKCU:\Environment' -Name 'Path').Path
+
+# Check if the directory .yen\bin exists in the user's PATH
+if (-not ($userPath -like "*$yenpath*")) {
+    # If it doesn't exist, append $yenpath to the user's PATH
+    $newPath = "$userPath;$yenpath"
+    Set-ItemProperty -Path 'HKCU:\Environment' -Name 'Path' -Value $newPath
+}
+
+Write-Host "Successfully installed yen! Restart the shell to start using the 'yen' command."
