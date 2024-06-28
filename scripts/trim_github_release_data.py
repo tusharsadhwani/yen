afe7162b86c4cf4fa105071817e63e63ba072994
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2024-06-28 19:15:18 +0530

    Fix script, trim release datas

--- scripts/trim_github_release_data.py
+++ scripts/trim_github_release_data.py
@@ -1,10 +1,20 @@
 import json
+import sys
 from yen.github import trim_github_release_data
 
 
-def generate_trimmed_release_data(filename: str) -> None:
+def save_trimmed_release_data(filename: str) -> None:
     with open(filename) as file:
         data = json.load(file)
 
     trimmed_data = trim_github_release_data(data)
-    print(json.dumps(trimmed_data, indent=2))
+    with open(filename, "w") as file:
+        json.dump(trimmed_data, file, indent=2)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.orig_argv[0]} {sys.orig_argv[1]} filepath.json")
+        sys.exit(1)
+
+    save_trimmed_release_data(sys.argv[1])
